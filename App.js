2383d4a4e4d3c145e1cecaabb27500422cb11cae
Alexander Shabanov <dffleh1995@gmail.com>
2022-12-20 18:58:20 +0200

    add form

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// <StatusBar style="auto" />
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableWithoutFeedback, // импорт компонента обертки
+  Keyboard, // импорт компонента клавиатуры
+  KeyboardAvoidingView, // новый импорт
+  Platform, // новый импорт
+  Alert,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const nameHandler = (text) => setName(text);
+  const passwordHandler = (text) => setPassword(text);
+
+  const onLogin = () => {
+    Alert.alert("Credentials", `${name} + ${password}`);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView // определяем ОС и настраиваем поведение клавиатуры
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <TextInput
+            value={name}
+            onChangeText={nameHandler}
+            placeholder="Username"
+            style={styles.input}
+          />
+          <TextInput
+            value={password}
+            onChangeText={passwordHandler}
+            placeholder="Password"
+            secureTextEntry={true}
+            style={styles.input}
+          />
+          <Button title={"Login"} style={styles.input} onPress={onLogin} />
+        </KeyboardAvoidingView>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "#ecf0f1",
+    marginBottom: 30,
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
   },
 });
